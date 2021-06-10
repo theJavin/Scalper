38e63b448495c5bbb63eafbaf3b2a4169f4ee2c3
Gavin McIntosh <gavinmcintosh01@gmail.com>
2021-06-10 15:04:49 -0500

    fixed add to cart feature

--- Scalper.py
+++ Scalper.py
@@ -4,28 +4,45 @@ import time
 
 from selenium import webdriver
 
-browser = webdriver.Chrome('/Documents/Projects/Scalper/chromedriver')
+browser = webdriver.Chrome('/home/particle/Documents/Mcprojects/Scalper/chromedriver')
 
 # Bestbuy 3070 ti page
 # https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-ti-8gb-gddr6x-pci-express-4-0-graphics-card-dark-platinum-and-black/6465789.p?skuId=6465789
 
-browser.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-ti-8gb-gddr6x-pci-express-4-0-graphics-card-dark-platinum-and-black/6465789.p?skuId=6465789")
+browser.get("https://www.bestbuy.com/site/apple-airpods-pro-white/5706659.p?skuId=5706659")
 
 buyButton = False
 
 while not buyButton:
+  try:
+      addToCartBtn = addButton = browser.find_element_by_class_name("btn-disabled")
+      print("Card not available.")
+      print("refreshing...")
+      time.sleep(1)  
+      browser.refresh()
 
-    try:
-        addToCartBtn = addButton = browser.find_element_by_class_name_("btn-disabled")
+  except:
+    addToCartBtn = addButton = browser.find_element_by_class_name("btn-primary")
+    addToCartBtn.click()
+    print("Card added to cart!")
+    buyButton = True
+    time.sleep(3)
+    
+try:
+  goToCartBtn = addButton = browser.find_element_by_class_name("go-to-cart-button")
+  goToCartBtn.click()
+  print("Going to cart!")
+  time.sleep(3)
 
-        print("button is not ready yet")
+except:
+  browser.quit()
 
-        time.sleep(0.5)
-        browser.refresh()
+try:
+  checkoutBtn = addButton = browser.find_element_by_class_name("btn-lg")
+  checkoutBtn.click
+  print("Checking out!")
+  time.sleep(2)
 
-    except:
-        addToCartBtn = addButton = browser.find_element_by_class_name("btn-primary")
-        print("button was clicked")
-        addToCartBtn.click()
-        
+except:
+  browser.quit()
 
